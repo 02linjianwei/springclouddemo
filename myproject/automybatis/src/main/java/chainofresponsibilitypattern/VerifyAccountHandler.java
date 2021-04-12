8affad09dfa3171f584b304ad2aecb4dec281856
linjianwei <linjianwei@tansun.com.cn>
2021-04-12 23:29:21 +0800

    create redisdomo

--- myproject/automybatis/src/main/java/chainofresponsibilitypattern/VerifyAccountHandler.java
+++
@@ -1,9 +0,0 @@
-package chainofresponsibilitypattern;
-
-public class VerifyAccountHandler extends  BuildHandler {
-    @Override
-    public void doHandler(LoginUser loginUser) {
-        System.out.println("====VerifyAccountHandler======");
-        next.doHandler(loginUser);
-    }
-}
--- myproject/automybatis/src/main/java/chainofresponsibilitypattern/VerifyRoleHanlder.java
+++
@@ -1,9 +0,0 @@
-package chainofresponsibilitypattern;
-
-public class VerifyRoleHanlder extends BuildHandler {
-    @Override
-    public void doHandler(LoginUser loginUser) {
-        System.out.println("========VerifyRoleHanlder============");
-        next.doHandler(loginUser);
-    }
-}
